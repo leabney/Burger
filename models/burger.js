eee1adfd7a09a2dfe0e8e9f3f98810aa25611da1
unknown <leabney@gmail.com>
2018-05-04 19:29:18 -0500

    working app

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require('./../config/orm');
+
+var burger = {
+    selectAll: function (callBack) {
+        orm.selectAll("burgers", function (result) {
+            callBack(result);
+        });
+    },
+    insertOne: function(columns,values,callBack) {
+        orm.insertOne("burgers",columns,values,function(result){
+            callBack(result);
+        })
+    },
+    updateOne: function(objColVals,condition,callback) {
+        orm.updateOne("burgers",objColVals,condition, function(result){
+            callback(result);
+        })
+    }
+}
+
+module.exports = burger;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    $("#submit").on("click", function(event) {
+        
+        event.preventDefault();
+    
+        
+        var newBurger = {
+          name: $("#name").val().trim(),
+          devoured: 0
+        };
+    
+        
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+
+        $("#name").val("");
+      });
+
+
+      $(".change-status").on("click", function(event) {
+        var id = $(this).data("id");
+       
+    
+        var newBurgerState = {
+          devoured: 1
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newBurgerState
+        }).then(
+          function() {
+            console.log("devoured burger");
+            // Reload the page to get the updated list
+          }
+        );
+        location.reload();
+      });
+
+})
